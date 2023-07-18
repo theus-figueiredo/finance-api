f8bcf51a8e0793463eea866c25590fcaf6507ea4
theus-figueiredo <matheus.b.figueiredo@outlook.com>
2023-07-18 15:10:26 -0300

    criados controllers e escritas as funções de CRUD de user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    private $user;
+
+    public function __construct(User $user) {
+        $this->user = $user;
+    }
+
+    public function index() {
+        $users = $this->user->paginate('10');
+
+        return response()->json(['data' => $users], 200);
+    }
+
+
+    public function store(Request $request) {
+        $data = $request->all();
+
+        if(!$request->has('password') || !$request->get('password')) {
+            return response()->json(['data' => 'User requires a passoword'], 400);
+        }
+
+        try {
+            $data['password'] = bcrypt($data['password']);
+
+            $user = $this->user->create($data);
+
+            return response()->json(['data' => $user], 201);
+
+        } catch(\Exception $e) {
+            return response()->json(['data' => $e->getMessage()], 400);
+        }
+    }
+
+
+    public function show(string $id) {
+        try {
+            $user = $this->user->findOrFail($id);
+
+            return response()->json(['data' => $user], 200);
+
+        } catch(\Exception $e) {
+            return response()->json(['data' => $e->getMessage()], 404);
+        }
+    }
+
+
+    public function update(Request $request, string $id) {
+        $data = $request->all();
+
+        if(!$request->has('password') || !$request->get('password')) {
+            return response()->json(['data' => 'User requires a passoword'], 400);
+        } else {
+            unset($data['password']);
+        }
+
+        try {
+            $user = $this->user->findOrFail($id);
+
+            $user->update($data);
+
+            return response()->json(['data' => $user], 202);
+
+        } catch(\Exception $e) {
+            return response()->json(['data' => $e->getMessage()], 400);
+        }
+    }
+
+
+    public function destroy(string $id) {
+        try {
+            $user = $this->user->findOrFail($id);
+
+            $user->delete();
+
+            return response()->json(['data' => 'deleted'], 200);
+
+        } catch(\Exception $e) {
+            return response()->json(['Error' => $e->getMessage()], 400);
+        }
+    }
+}
